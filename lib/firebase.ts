19772b7e57891cb5b6878b42599c576f2ae3349e
Lakshmi Niranjan <88361371+lakshminiranjan@users.noreply.github.com>
2026-02-24 22:31:16 +0530

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCNnCCSNDfy6a45WaMAW37IKd5DeJ16vzc",
-  authDomain: "emimobiletracker.firebaseapp.com",
-  projectId: "emimobiletracker",
-  storageBucket: "emimobiletracker.firebasestorage.app",
-  messagingSenderId: "1061024235190",
-  appId: "1:1061024235190:web:e5194802d11472d4f6f017",
-  measurementId: "G-TN875FTJSD",
+  apiKey: "xxxxxxxxxxxxxxxxxxxxx",
+  authDomain: "xxxxxxxxxxxxxxxxxx",
+  projectId: "xxxxxxxxxxxxxxx",
+  storageBucket: "xxxxxxxxxxxxxxxxx",
+  messagingSenderId: "xxxxxxxxxxxx",
+  appId: "xxxxxxxxxxxxxxxxxxxxx",
+  measurementId: "xxxxxxxxxx",
 };
 
 const app = initializeApp(firebaseConfig);
